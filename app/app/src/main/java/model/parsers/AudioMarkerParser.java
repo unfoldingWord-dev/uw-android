c220feaec6e115797f4eebe8da26d18cf5221623
PeeJWeeJ <pj.fechner@gmail.com>
2015-10-02 15:28:25 -0500

    Fixing and improving a few layout issues.

---
+++ app/app/src/main/java/model/parsers/AudioMarkerParser.java
@@ -0,0 +1,13 @@
+package model.parsers;
+
+import android.net.Uri;
+
+/**
+ * Created by Fechner on 10/2/15.
+ */
+public class AudioMarkerParser {
+
+    public static void createAudioMarkers(Uri audioUri){
+
+    }
+}
